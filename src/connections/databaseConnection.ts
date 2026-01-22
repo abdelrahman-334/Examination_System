3b65c1ae602bc9f7705ae55c06b1cbb3a5091636
abdelrahman-334 <omarnad141070@gmail.com>
2026-01-22 20:21:52 +0200

    update connection function

--- src/connections/databaseConnection.ts
+++ src/connections/databaseConnection.ts
@@ -7,15 +7,15 @@ const sqlConfig = {
  user: process.env.DB_USER,
   password: process.env.DB_PWD,
   database: process.env.DB_NAME,
-  server: 'localhost\\SQLEXPRESS',
+  server: process.env.DB_SERVER || '.\SQLEXPRESS',
   pool: {
     max: 10,
     min: 0,
     idleTimeoutMillis: 30000
   },
   options: {
-    encrypt: false,
-    trustServerCertificate: true // change to true for local dev / self-signed certs
+    encrypt: true,
+    trustServerCertificate: false // change to true for local dev / self-signed certs
   }
 }
 
